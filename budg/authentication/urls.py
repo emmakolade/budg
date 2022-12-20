3f746233bdf438807b5f07a796f7b1e346017c28
Akolade <emmakolade@gmail.com>
2022-12-20 11:40:59 +0100

    password reset

--- budg/authentication/urls.py
+++ budg/authentication/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from .views import Registration, UsernameValidation, Login, Logout
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
 
@@ -15,9 +16,15 @@ urlpatterns = [
     # path('email_validate/', csrf_exempt(EmailValidation.as_view()), name="email_validate"),
 
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
-        template_name='main/authentication/password_reset_done.html'), name='password_reset_done'),
+        template_name='authentication/password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name="main/authentication/password_reset_confirm.html"), name='password_reset_confirm'),
+        template_name="authentication/password_reset_confirm.html"), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='main/authentication/password_reset_complete.html'), name='password_reset_complete'),
+        template_name='authentication/password_reset_complete.html'), name='password_reset_complete'),
+    
+    path("password_reset", views.password_reset_request, name="password_reset")
+
+    # path('password_reset/', PasswordResetRequest.as_view(), name="password_reset"),
+    # path("password_reset", views.password_reset_request, name="password_reset")
+
 ]
--- budg/authentication/views.py
+++ budg/authentication/views.py
@@ -1,15 +1,19 @@
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import PasswordResetForm
 from django.shortcuts import render, redirect
 from django.views import View
 import json
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.core.validators import validate_email
-
-from django.core.mail import EmailMessage
+from django.core.mail import send_mail, BadHeaderError, EmailMessage
+from django.template.loader import render_to_string
+from django.db.models.query_utils import Q
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.encoding import force_bytes
 
 
 class Registration(View):
@@ -91,6 +95,36 @@ class Logout(View):
         logout(request)
         messages.success(request, 'you have been logged out')
         return redirect('base')
-    
 
 
+# password reset
+def password_reset_request(request):
+    if request.method == "POST":
+        password_reset_form = PasswordResetForm(request.POST)
+        if password_reset_form.is_valid():
+            data = password_reset_form.cleaned_data['email']
+            associated_user = User.objects.filter(Q(email=data))
+            if associated_user.exists():
+                for user in associated_user:
+                    subject = "password reset requested"
+                    email_template_name = "authentication/password_reset_email.txt"
+                    email_info = {
+                        'email': user.email,
+                        'domain': '127.0.0.1:8000',
+                        'site_name': 'Budg',
+                        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                        'user': user,
+                        'token': default_token_generator.make_token(user),
+                        'protocol': 'http',
+                    }
+
+                    email = render_to_string(email_template_name, email_info)
+                    try:
+                        send_mail(subject, email, 'admin@example.com',
+                                  [user.email], fail_silently=False)
+                    except BadHeaderError:
+                        return HttpResponse('Invalid header found.')
+                    return redirect("/password_reset/done/")
+
+    password_reset_form = PasswordResetForm()
+    return render(request, 'authentication/password_reset.html')
